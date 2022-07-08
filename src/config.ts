d5d5935e8c7c560af6839a3d3b737b94bdba6c38
Barış Demirci <demirci.baris38@gmail.com>
2022-07-08 09:31:09 +0300

    🔧 Change seasonal ranked type

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,12 @@ export default {
 	API_VERSION: "/v1",
 	BH_API_BASE: "https://api.brawlhalla.com",
 	BH_API_KEY: process.env.BH_API_KEY as string,
-	PORT: (process.env.PORT as unknown) as number,
+	PORT: process.env.PORT as unknown as number,
 	MONGODB_URI: process.env.MONGODB_URI as string,
 	SYNC_PERIOD: 1000 * 60 * 15,
 	SYNC_RATELIMIT: {
 		points: 1,
 		duration: 60 * 2,
 	},
-	SEASONAL_RANKED: "brawlball",
+	SEASONAL_RANKED: "switchcraft",
 };
