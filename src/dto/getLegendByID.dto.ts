9617133d900d01dd2051595c4f014dc6090f299c
Barış DEMİRCİ <github@338.rocks>
2024-05-03 15:44:19 +0300

    new legend Vivi (deploy)

--- src/dto/getLegendByID.dto.ts
+++ src/dto/getLegendByID.dto.ts
@@ -2,14 +2,11 @@ import { Transform } from "class-transformer";
 import { IsDefined, IsNumber, Max, Min, NotEquals } from "class-validator";
 
 export class GetLegendByIDDTO {
-	// @NotEquals(27) new legend loki uses legend id 27
-	// @NotEquals(17) new legend red raptor uses legend id 17
-	// @NotEquals(62) new legend thea uses legend id 62
 	@IsDefined()
 	@Transform((i) => parseInt(i.value))
 	@IsNumber()
 	@Min(3)
-	@Max(63)
+	@Max(64)
 	@NotEquals(61)
 	legend_id: number;
 }
