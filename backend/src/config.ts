b35c8abe5c165fb922b6a93330d25980316fdd78
Barış DEMİRCİ <hi@338.rocks>
2025-08-11 19:10:36 +0300

    Better env management

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,21 @@
-import { config } from "dotenv";
+import { cleanEnv, num, port, str, url } from "envalid";
 
-config();
+const env = cleanEnv(process.env, {
+	API_PREFIX: str({ default: "/api" }),
+	PORT: port({ default: 5555 }),
+	TTL_MINS: num({ default: 15 }),
+	BH_API_KEY: str({
+		desc: "API key from Brawlhalla",
+	}),
+	REDIS_URL: url({
+		desc: "Redis URL",
+	}),
+});
 
 export default {
-	API_PREFIX: process.env.API_PREFIX ?? "/api",
-	PORT: parseInt(process.env.PORT ?? "5555", 10),
-	TTL_MINS: parseInt(process.env.TTL_MINS ?? "15", 10),
-	BH_API_KEY: process.env.BH_API_KEY,
-	REDIS_URL: process.env.REDIS_URL,
+	API_PREFIX: env.API_PREFIX,
+	PORT: env.PORT,
+	TTL_MINS: env.TTL_MINS,
+	BH_API_KEY: env.BH_API_KEY,
+	REDIS_URL: env.REDIS_URL,
 };
